e644141ba4e684c0a177659de549d3322bcd68ae
ekusuy <ekusuy.omi@gmail.com>
2019-05-14 16:37:35 +0900

    contactを追加

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
 {
-    //
+  public function contact()
+  {
+    return view("contact");
+  }
+
+  public function index()
+  {
+    return view('welcome');
+  }
 }
