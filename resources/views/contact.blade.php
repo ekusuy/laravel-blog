ec576a7a76730e04c48f41284993cf57366d4310
ekusuy <ekusuy.omi@gmail.com>
2019-05-14 17:35:16 +0900

    layoutに合わせてviewを書き換え

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>contact</title>
-</head>
-<body>
+@extends('layout')
+
+@section('content')
   <h1>contact me!</h1>
-</body>
-</html>
+@endsection
