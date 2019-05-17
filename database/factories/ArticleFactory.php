e03ae47f7cbb2c15c8b9b192a31f4e80bef89a23
ekusuy <ekusuy.omi@gmail.com>
2019-05-17 12:06:38 +0900

    factoryとseederを実装

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -2,11 +2,10 @@
 
 /* @var $factory \Illuminate\Database\Eloquent\Factory */
 
-use App\Model;
 use Faker\Generator as Faker;
 use Carbon\Carbon;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(App\Article::class, function (Faker $faker) {
     return [
       'title' => $faker->sentence(),
       'body' => $faker->paragraph(),
