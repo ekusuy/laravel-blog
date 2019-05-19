5fff3495abd49233b881ad57de561af052f9b321
ekusuy <ekusuy.omi@gmail.com>
2019-05-19 21:14:29 +0900

    indexとshowを追加

---
+++ app/Http/Controllers/ArticlesController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Article;
+
+class ArticlesController extends Controller
+{
+  public function index(){
+    $articles = Article::all();
+
+    return view('articles.index', compact('articles'));
+  }
+
+  public function show($id){
+    $article = Article::findOrFail($id);
+    return view('articles.show', compact('article'));
+  }
+
+  public function create(){
+    return view('articles.create');
+  }
+
+  public function store(Request $request){
+    $rules = [
+      'title' => 'required|min:3',
+      'body' => 'required',
+      'published_at' => 'required|date',
+    ];
+    $validated = $this->validate($request, $rules);
+
+    Article::create($validated);
+
+    return redirect('articles');
+  }
+}
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,10 +5,17 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>My Blog</title>
+  <!-- ① CSS を追加 -->
+  <link rel="stylesheet" href="/css/app.css">
+
+  <!-- ② JavaScript を追加 -->
+  <script src="/js/app.js" defer></script>
 </head>
 <body>
 
-  @yield('content')
+  <div class="container py-4">
+    @yield('content')
+  </div>
 
 </body>
 </html>
