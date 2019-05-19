9ac4ec3e9e38485b6beedf2152a13540f265dc15
ekusuy <ekusuy.omi@gmail.com>
2019-05-20 01:43:09 +0900

    delete追加

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -44,4 +44,12 @@ class ArticlesController extends Controller
 
     return redirect(url('articles', [$article->id]));
   }
+
+  public function destroy($id) {
+    $article = Article::findOrFail($id);
+
+    $article->delete();
+
+    return redirect('articles');
+  }
 }
--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -14,6 +14,8 @@
   <div>
     <a href="{{ action('ArticlesController@edit', [$article->id]) }}" class="btn btn-primary">編集</a>
 
+    {!! delete_form(['articles', $article->id]) !!}
+
     <a href="{{ action('ArticlesController@index') }}" class="btn btn-secondary float-right">一覧へ戻る</a>
   </div>
 @endsection
